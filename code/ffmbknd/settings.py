8eb03a500c9c1dc452d2ac11d1b6cf33d212cb00
khangerelganzorig <khangerel.g@mak.mn>
2023-05-16 12:49:31 +0800

    python-3

--- code/ffmbknd/settings.py
+++ code/ffmbknd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^l)7d*%h&db4uft@dk%h-w&nup#pu%)a!d)c7jwgoixo5_hm0$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '43.202.40.53', 'farofoundation.org', 'www.farofoundation.org']
 
