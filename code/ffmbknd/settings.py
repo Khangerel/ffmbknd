1e67187c94f23f9c85d7fec3dd0a06a3d263b280
khangerelganzorig <khangerel.g@mak.mn>
2023-05-16 15:06:35 +0800

    nginx-13

--- code/ffmbknd/settings.py
+++ code/ffmbknd/settings.py
@@ -134,10 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/' 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_IMAGE_BACKEND = 'pillow'
 
