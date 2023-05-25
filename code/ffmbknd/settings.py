1e5eddd560fa4ecd5f8a5412480a564f2b29d4e1
khangerelganzorig <khangerel.g@mak.mn>
2023-05-25 10:55:33 +0800

    cors-5

--- code/ffmbknd/settings.py
+++ code/ffmbknd/settings.py
@@ -63,11 +63,18 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://farofoundation.org',
+    'https://www.farofoundation.org',
+]
+
+CSRF_COOKIE_DOMAIN = 'https://farofoundation.org'
 
 CORS_ALLOWED_ORIGINS = [
     'http://192.168.2.22:3000',
