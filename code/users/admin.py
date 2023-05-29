062a4a84b3599a12e178e39d82b151b9dd0ecdd1
khangerelganzorig <khangerel.g@mak.mn>
2023-05-29 15:06:14 +0800

    superuser

--- code/users/admin.py
+++ code/users/admin.py
@@ -1,5 +1,9 @@
-from django.contrib import admin
 from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
 from . import models
 
@@ -11,20 +15,83 @@ class UserAdminForm(forms.ModelForm):
         fields = "__all__"
 
 
-class UserAdmin(admin.ModelAdmin):
-    form = UserAdminForm
-    list_display = [
-        "first_name",
-        "last_name",
-        "email",
-        "is_staff",
-        "created",
-        "last_updated",
-    ]
-    readonly_fields = [
-        "created",
-        "last_updated",
-    ]
+class CustomUserAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = models.CustomUser
+        fields = "__all__"
 
 
-admin.site.register(models.CustomUser, UserAdmin)
+class CustomUserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = models.CustomUser
+        fields = ["email"]
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+    
+class CustomUserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = models.CustomUser
+        fields = ["email", "password", "is_active"]
+
+class CustomUserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["email", "is_staff"]
+    list_filter = ["is_staff"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields":[]}),
+        ("Permissions", {"fields": ["is_staff", "is_superuser", 'groups', 'user_permissions']}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = ('groups', 'user_permissions')
+
+
+admin.site.register(models.CustomUser, CustomUserAdmin)
--- code/users/models.py
+++ code/users/models.py
@@ -5,17 +5,31 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            **extra_fields
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
     
 # comment added
 
