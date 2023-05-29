d9157ad9ac92614cbcae04a9e10e29fce37769d8
khangerelganzorig <khangerel.g@mak.mn>
2023-05-29 19:37:41 +0800

    debug=false

--- code/ffmbknd/settings.py
+++ code/ffmbknd/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^l)7d*%h&db4uft@dk%h-w&nup#pu%)a!d)c7jwgoixo5_hm0$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'farofoundation.org', 'www.farofoundation.org']
 
