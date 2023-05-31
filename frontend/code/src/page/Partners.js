43db8d49b8008dc99c11c052d3471fa0ff0ef950
Tuvshuu <tuvshinzaya.a@mak.mn>
2023-05-31 16:00:22 +0800

    :

--- frontend/code/src/page/Partners.js
+++ frontend/code/src/page/Partners.js
@@ -63,10 +63,10 @@ function Partners() {
         <p className="text-gray text-center mb-5">{t("landing.partner_description")}</p>
         <Row className="bg-white d-flex align-items-center justify-content-center w-100">
           {our_main_data.map((partner) => (
-            <Col className="align-items-center pb-1" xs={12} sm={12} md={6} lg={4} xl={3}>
+            <Col className="align-items-center pb-5" xs={12} sm={12} md={6} lg={4} xl={3}>
               <Image
                 src={partner.logo}
-                className="w-100 contain"
+                className="w-100 contain p-4"
                 onClick={() => handleClick()}
               />
             </Col>
